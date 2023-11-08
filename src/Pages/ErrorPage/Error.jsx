51ba49d39c28a22d3fb53f2077bb4daf65c794e3
Farida Yasmeen <faridayasmeencse@gmail.com>
2023-11-09 05:20:26 +0600

    added error element page

--- src/Pages/ErrorPage/Error.jsx
+++ src/Pages/ErrorPage/Error.jsx
@@ -1,11 +1,13 @@
-
+import { Link } from 'react-router-dom';
+import errorpage from '../../errorpage.json'
+import Lottie from 'lottie-react';
 
 const Error = () => {
     return (
         <div className="relative w-full">
-            <img className="w-96" src="https://i.ibb.co/6NnVFmB/error.png" alt="" />
-            <button className="absolute top-1/2 left-40 mx-auto py-10 
-                btn-warning bg-amber-400 w-60 h-8">Go Home</button>
+            <Lottie className='w-[40%] h-auto mx-auto' animationData={errorpage} />
+            <Link to='/'><button className="absolute top-[85%] left-[40%] mx-auto 
+                btn-warning bg-amber-400 w-60 h-16 rounded-2xl font-bold text-2xl">Go Home</button></Link> 
         </div>
     );
 };
